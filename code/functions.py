8e0ac5d04e7f07089dbb42ca72344cf0d2cbafe6
CALLEJA CARDIEL CARLOS <ebt2057@axa-seguros-es.intraxa>
2025-05-07 14:27:28 +0200

    Added an explanation of what this module does

--- code/functions.py
+++ code/functions.py
@@ -9,9 +9,13 @@ def load_documents(dir:str):
 
 #Updated loading function
 def load_documents_updated(dir:str,text_template:str,excluded_embed_keys:list, excluded_llm_keys:list):
+    #Load the data
     docs=SimpleDirectoryReader(input_dir=dir).load_data()
+    #Edits how the information and metadata is passed to the embeding model and llm model
     for doc in docs:
         doc.text_template= text_template
+        #The keys that are excluded for the embeding model
         doc.excluded_embed_metadata_keys= excluded_embed_keys
+        #The keys that are excluded for the llm model
         doc.excluded_llm_metadata_keys= excluded_llm_keys
     return docs
